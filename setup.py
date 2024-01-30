8de6850dcc2fce92ac01ce1c9e299560157143db
amitbharti017 <amitbharti017@gmail.com>
2024-01-30 22:23:59 +0530

    requirements and setup done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "waste_detection",
+    version = "0.0.0",
+    author = "Amit Bharti",
+    author_email = "amitbharti936@gmail.com",
+    packages = find_packages(),
+    install_requires = []
+)
